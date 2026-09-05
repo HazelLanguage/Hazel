f5bd992169378692e0be2080c6d9b848208fe5a3
AlinaWan <wuangelina40@gmail.com>
2026-09-05 11:37:59 -0700

    refactor(codegen): fully qualify Int128 types with System namespace

--- src/Hazel/CodeGen/CSharp/CSharpTypeExtensions.cs
+++ src/Hazel/CodeGen/CSharp/CSharpTypeExtensions.cs
@@ -18,8 +18,8 @@ public static class CSharpTypeExtensions
             "integer64" => "long",
             "uinteger64" => "ulong",
 
-            "integer128" => "Int128",
-            "uinteger128" => "UInt128",
+            "integer128" => "System.Int128",
+            "uinteger128" => "System.UInt128",
 
             "character" => "char",
             "string" => "string",
